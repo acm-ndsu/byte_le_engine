3d85900e2f7c39934ae584890448eb8153edc490
MyFridgeIsFinePleaseStopAsking <101206316+JuliaCaesar@users.noreply.github.com>
2023-05-05 22:05:24 -0500

    Update test_player.py

--- game/test_suite/tests/test_player.py
+++ game/test_suite/tests/test_player.py
@@ -3,6 +3,7 @@ from game.common.player import Player
 from game.common.game_object import GameObject
 from game.common.avatar import Avatar
 from game.common.enums import *
+from game.common.avatar import Avatar
 
 
 class TestPlayer(unittest.TestCase):
@@ -11,4 +12,13 @@ class TestPlayer(unittest.TestCase):
         self.object_type: ObjectType = ObjectType.PLAYER
         self.functional: bool = True
         self.player: Player = Player
-        self.
+        self.actions: list[ActionType] = []
+        self.team_name: str | None = ""
+        self.avatar: Avatar | None = Avatar
+
+    
+
+
+
+
+
